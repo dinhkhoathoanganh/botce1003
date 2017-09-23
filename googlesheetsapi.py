e3c5c173e53fee3abc083421af3c9f3da8170a33
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-23 16:39:24 +0800

    Update googlesheetsapi.py

--- googlesheetsapi.py
+++ googlesheetsapi.py
@@ -4,7 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
  
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('canteen_restaurant_list.json', scope)
 client = gspread.authorize(creds)
  
 # Find a workbook by name and open the first sheet
