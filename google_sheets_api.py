f167063e0761a26a601059c103b86d9f9c4cdd40
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-23 21:17:51 +0800

    Add files via upload

--- google_sheets_api.py
+++ google_sheets_api.py
@@ -13,10 +13,12 @@ sheet = client.open("Canteen / Restaurant List").sheet1
  
 # Extract and print all of the values
 print(sheet.get_all_values())
-# write 'Blue' to C2 cell
-sheet.update_acell('C2', 'Blue')
+# write 'Green' to C2 cell
+sheet.update_acell('C2', 'Green')
 # reading 
 all_cells = sheet.range('A1:C6')
 print(all_cells)
 for cell in all_cells:
- print (cell.value)
+ print(cell.value)
+
+
