3887bb6cbb322658e44e46cbc616386e279365b2
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-24 16:52:01 +0800

    Add files via upload

--- mainbot.py
+++ mainbot.py
@@ -2,6 +2,22 @@ import time
 import datetime
 import telepot
 from telepot.loop import MessageLoop
+#sendmsg
+def sendmsg(userid,content):
+ bot.sendMessage(userid,conten)
+#userid
+def userid(msg):
+ content_type, chat_type, chat_id = telepot.glance(msg)
+ return chat_id
+#username
+def username(msg):
+ return msg['from']['first_name']+msg['from']['last_name']
+#
+
+
+
+
+
 #receive message
 def handle(msg):
  content_type, chat_type, chat_id = telepot.glance(msg)
@@ -13,17 +29,20 @@ def handle(msg):
 #answer a msg with 'dit me'
  if True:
   bot.sendMessage(chat_id, 'dit me '+msgfrom['first_name']+' '+msgfrom['last_name'])
+#print the time
   bot.sendMessage(chat_id,'bay gio la may gio nhi? :'+str(datetime.datetime.now()))
+
+
+
+
+
+
+
+
+
+
 #bot started
 bot = telepot.Bot('446414243:AAG13E9L9ifrrYJc0JNHIHMpHBK-306sd2A')#paste the key here
 bot.message_loop(handle)
 print ('Listening ...')
-
-
-
-
-
-# Keep the program running.
-while 1:
- time.sleep(0)
 
