d3edf27aa18ea520689e72ac042af93d687dc128
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-24 19:38:58 +0800

    Add files via upload

--- excel_final.py
+++ excel_final.py
@@ -2,21 +2,25 @@ from openpyxl import load_workbook
 import datetime
 #load workbook from a file and grab the active worksheet
 work_book=load_workbook('canteen_restaurant_list.xlsx')
-work_sheet=work_book.active
+work_sheet=work_book['Sheet2']
 
 #return cell value function
 def cellvalue(x): #x is the cell address(str)
  return work_sheet[x].value
 
 #save the file
-def save_xl(x):
- work_book.save("canteen_restaurant_list.xls")
+def save_xl(z): #z is the file name
+ work_book.save(z)
 
 #write datetime to cell
 def write_datetime(x): #x is the cell address(str)
  work_sheet[x] = datetime.datetime.now()
 
 #assign data
-def assign(x,y):
+def assign(x,y): #x is the cell address(str)
  work_sheet[x]=y
+
+#program starts from here
+assign('A1',20)
+save_xl("canteen_restaurant_list.xlsx")
 
