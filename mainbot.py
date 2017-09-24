2f3b816c7d843a962693701b65dcfc77fd86332a
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-25 07:31:30 +0800

    Add files via upload

--- mainbot.py
+++ mainbot.py
@@ -1,15 +1,13 @@
+
+import sys
 import time
-import threading
-import random
 import telepot
 from telepot.loop import MessageLoop
-from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, ForceReply
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from telepot.namedtuple import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent
 
 
 
-#print message content
+#message content
 def content_msg(msg):
  return msg['text'].lower()
 
@@ -19,31 +17,36 @@ def send_msg(chat_id, content):#content(string)
  bot.sendMessage(chat_id,content)
 
 
+#send a inlinekeyboard
+def inline_mcq(chat_id, question,choice_number, choices):
+#question(string) choice_number(integer) choices(list)
+#example for an array: (['press 1',1],['press 2',2],['press 3',3])
+#(1) is a list or an integer, (1,) is a tuple
+ inline_keyboard = [[InlineKeyboardButton(text=choices(0), callback_data=choices(0)],]
+ for i in range(1,choice_number)
+  inline_keyboard = inline_keyboard + [[InlineKeyboardButton(text=choices(i), callback_data=choices(i)]]
+ keyboard = InlineKeyboardMarkup(inline_keyboard)
 
-#send a multiple-choice question
-def send_mcq(chat_id, question, keyboard_list):#question(string) keyboard_list is a list including elements with forms <KeyboardButton(text='<string name of the button>')>. Example: [KeyboardButton(text='Yes'),KeyboardButton(text='No')].
- bot.sendMessage(chat_id, question, reply_markup=ReplyKeyboardMarkup(keyboard=[keyboard_list]))
-
-#request location
-def request_location(chat_id):
- markup = ReplyKeyboardMarkup(keyboard=[
-                     ['Plain text', KeyboardButton(text='Text only')],
-                     [dict(text='Phone', request_contact=True), KeyboardButton(text='Location', request_location=True)],
-                 ])
- bot.sendMessage(chat_id, 'Share your location to get the direction', reply_markup=markup)
-
+ bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
 
 
-#call back
-def call_back():
- mainbot()
+    
 
-#Mainbot starts from here
-def mainbot(msg):
+def on_callback_query(msg):
+ query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+ print('Callback Query:',query_data)
+
+ bot.answerCallbackQuery(query_id, text='Got it')
+
+#mainbot
+def on_chat_message(msg):
  content_type, chat_type, chat_id = telepot.glance(msg)
- if content_msg(msg) == 'hello':
-  request_location(chat_id)
- send_msg(chat_id, 'done')
+#test
+ inline_mcq(chat_id,'this is the question',3,(1,2,3))
+#endtest
+
+
+
 
 
 
@@ -53,10 +56,9 @@ def mainbot(msg):
 
 
 bot = telepot.Bot('446414243:AAG13E9L9ifrrYJc0JNHIHMpHBK-306sd2A')
-MessageLoop(bot,{'chat':mainbot,'call_back': call_back}).run_as_thread()
-print ('Listening ...')
+MessageLoop(bot, {'chat': on_chat_message,
+                  'callback_query': on_callback_query}).run_as_thread()
+print('Listening ...')
 
-
-# Keep the program running.
 while 1:
- time.sleep(10)
+    time.sleep(10)
