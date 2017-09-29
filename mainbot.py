0d760e0d5c249deaffad95568d32b68fdc8ee12f
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-29 22:31:01 +0800

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -15,23 +15,15 @@ def receive_msg(msg):
 def send_msg(chat_id, content):#content(string)
  bot.sendMessage(chat_id,content)
 
-#send a custom keyboard
-def custom_key(chat_id, question, choices_list): #choices_list must be a list of string.
- execmarkup = ''
- for i in range(len(choices_list)):
-  execmarkup = execmarkup + '['+ choices_list[i]+'],'
- execmarkup0 = 'markup = ReplyKeyboardMarkup(keyboard=[' + execmarkup + '])'
- exec(execmarkup0)
- bot.sendMessage(chat_id, question, reply_markup=markup)
+#send a custom keyboard example
+def custom_key()
+  markup = ReplyKeyboardMarkup(keyboard=[['Plain text'],['Testing text'],])
+  bot.sendMessage(chat_id, 'Custom keyboard with various buttons', reply_markup=markup)
 #mainbot
 def main(msg):
  content_type, chat_type, chat_id = telepot.glance(msg)
 #start coding from here
- send_msg(chat_id, 'testing')
- print(receive_msg(msg))
- if receive_msg(msg) == 'hello':
-  markup = ReplyKeyboardMarkup(keyboard=[['Plain text'],['Testing text'],])
-  bot.sendMessage(chat_id, 'Custom keyboard with various buttons', reply_markup=markup)
+
 
 
 bot = telepot.Bot('446414243:AAG13E9L9ifrrYJc0JNHIHMpHBK-306sd2A')
