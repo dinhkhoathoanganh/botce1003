b5fc2f26bcc7a8e06be7a9d47733613ef9e27e30
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-29 22:33:06 +0800

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -19,6 +19,10 @@ def send_msg(chat_id, content):#content(string)
 def custom_key()
   markup = ReplyKeyboardMarkup(keyboard=[['Plain text'],['Testing text'],])
   bot.sendMessage(chat_id, 'Custom keyboard with various buttons', reply_markup=markup)
+#remove keyboard
+def remove_key()
+ markup = ReplyKeyboardRemove()
+ bot.sendMessage(chat_id, 'Hide custom keyboard', reply_markup=markup)
 #mainbot
 def main(msg):
  content_type, chat_type, chat_id = telepot.glance(msg)
