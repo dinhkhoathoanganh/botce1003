5e5dbc2c4d21ac5ea142d93d1b2ce3882dc75045
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2017-09-29 20:52:26 +0800

    Add files via upload

--- mainbot.py
+++ mainbot.py
@@ -17,19 +17,21 @@ def send_msg(chat_id, content):#content(string)
 
 #send a custom keyboard
 def custom_key(chat_id, question, choices_list): #choices_list must be a list of string.
- keyboard = [[choices_list[0]],]
- for i in range(1,len(choices_list)):
-  keyboard = keyboard + [choices_list[i]]
- bot.sendMessage(chat_id, question, reply_markup=ReplyKeyboardMarkup(keyboard))
-  
-
+ execmarkup = ''
+ for i in range(len(choices_list)):
+  execmarkup = execmarkup + '['+ choices_list[i]+'],'
+ execmarkup0 = 'markup = ReplyKeyboardMarkup(keyboard=[' + execmarkup + '])'
+ exec(execmarkup0)
+ bot.sendMessage(chat_id, question, reply_markup=markup)
 #mainbot
 def main(msg):
  content_type, chat_type, chat_id = telepot.glance(msg)
 #start coding from here
- custom_key(chat_id, "testing question", ['1','2','3'])
-
-
+ send_msg(chat_id, 'testing')
+ print(receive_msg(msg))
+ if receive_msg(msg) == 'hello':
+  markup = ReplyKeyboardMarkup(keyboard=[['Plain text'],['Testing text'],])
+  bot.sendMessage(chat_id, 'Custom keyboard with various buttons', reply_markup=markup)
 
 
 bot = telepot.Bot('446414243:AAG13E9L9ifrrYJc0JNHIHMpHBK-306sd2A')
