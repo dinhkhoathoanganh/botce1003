ff23b5932b14e5dc7515599414cbab0b45311c6b
dinhkhoathoanganh <dinhkhoathoanganh@gmail.com>
2017-09-30 10:43:23 +0800

    Add files via upload
    Credentials file edited

---
+++ credentialshhhanh.py
@@ -0,0 +1,12 @@
+food2fork_key = '{YOUR API KEY}'
+food2fork_url = 'http://food2fork.com/api/search'
+puppy_url = 'http://www.recipepuppy.com/api/'
+
+yummly_id = '{YOUR API ID}'
+yummly_key = '{YOUR API KEY}'
+yummly_url = 'http://api.yummly.com/v1/api/recipes?'
+
+google_key = '{YOUR API KEY}'
+search_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+details_url = 'https://maps.googleapis.com/maps/api/place/details/json'
+direction_url = 'https://maps.googleapis.com/maps/api/directions/json' 
