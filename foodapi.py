6b3ee704cb50ed7cb8970e16b93c4fadab74f89b
dinhkhoathoanganh <dinhkhoathoanganh@gmail.com>
2017-09-30 10:34:52 +0800

    Add files via upload
    updated version unmerged puppy and food2fork

--- foodapi.py
+++ foodapi.py
@@ -1,24 +1,49 @@
-#Search engine based on Food2Fork https://food2fork.com/about/api and Recipe Puppy http://www.recipepuppy.com/about/api/
-#NOTE!!!! Food2Fork has limited total call before this api key expires ;( 5000 calls; Recipe puppy is free
+##Search engine based on Food2Fork https://food2fork.com/about/api and Recipe Puppy http://www.recipepuppy.com/about/api/
+##NOTE!!!! Food2Fork has limited total call before this api key expires ;( 5000 calls; Recipe puppy is free
 
 import requests
 import json
+import re
 
-#Food2fork API key
-food2fork_api = 'd6ec9ede5565ccaf1417242d82d1f537'
+from credentialshhanh import *
 
-#Set up excel file
+##Set up excel file
 filename = "products.csv"
 f = open(filename, "w") #open excel file
 
-#Select the search mode
+##Select the search mode
 def search_selection(choice):
     if choice == 1:
         query = input("Key in the dishname: ")
+        query = re.sub('[\\\\/:*?"<>|]', '', query)
         food2fork_search(query)
     elif choice == 2:
-        query = input("Key in the ingredients list, separated by comas: ")
-        puppy_search(query)
+        ingredients = input("Key in the ingredients you want, separated by comas: ")
+        ingredients = re.sub('[\\\\/:*?"<>|]', '', ingredients)
+
+##### NOTE: include in option Yes/No button in the bot for the below
+        # not_ingredients = input("Key in the ingredients you DONT want, separated by comas: ")
+
+        # if not_ingredients != '':
+        #     not_ingredients = re.sub('[\\\\/:*?"<>|  ]', ',', not_ingredients)
+        #     not_ingredients = not_ingredients.replace(",,",",")
+        #     not_ingredients = not_ingredients.replace(",",",-")
+        #     ingredients = ingredients + ",-" + not_ingredients
+
+        #print (ingredients)
+        puppy_search(ingredients)
+
+## API Call function
+def fetch_data(search_payload, url):
+    search_req = requests.get(url, params=search_payload)
+    print(search_req)
+    if search_req.status_code == requests.codes.ok:
+        data = search_req.json()
+        #print(data)
+        return data
+    else:
+        print("Error!")
+        exit()
 
 #Write data fetched from food2fork to excel file
 def food2fork_to_csv(data):
@@ -34,36 +59,29 @@ def puppy_to_csv(data):
     for item in data['results']:
         f.write(item['title'].replace(",","|")  + "," + item['href'] + ","  + item['thumbnail'] + "\n")
 
-#Edge case no data found
+##Edge case no data found
 def data_check(data, mode):
     if ((mode == 1) and (data['count'] == 0)) or ((mode == 2) and data['results'] == []):
-        print("No data found! Is there a typo?!")
+        print("What an odd combination! Is there a typo?!")
         return 0
     else: 
         return 1
 
-#Call food2fork for data
-def food2fork_search(query):
-    api_key = food2fork_api
-    url = 'http://food2fork.com/api/search?key=' + api_key
-    key_words = query.replace(' ', '%20')
-    final_url = url + "&q=" + key_words
-    json_obj = requests.get(final_url)
-    data = json_obj.json()
-##    print(data)
+##Call food2fork for data
+def food2fork_search(query, sort='', page=''):
+    
+    search_payload = {"key":food2fork_key, "q":query, "sort":sort, "page":page}
+    data = fetch_data(search_payload, food2fork_url)
     if data_check(data, 1) == 1:
         food2fork_to_csv(data)
+    
+##Call recipe puppy for data
+def puppy_search(ingredients, query='', page=''):
+    
+    ingredients = ingredients.split(",")
 
-#Call recipe puppy for data
-def puppy_search(query):
-##    ingrd_list = query.split(",")
-    url = 'http://www.recipepuppy.com/api/?i='
-    key_words = query.replace(' ', ',')
-    final_url = url + key_words
-##    print('hey! ' + final_url)
-    json_obj = requests.get(final_url)
-    data = json_obj.json()
-    print(data)
+    search_payload = {"q":query, "i":ingredients, "page":page}
+    data = fetch_data(search_payload, puppy_url)
     if data_check(data, 2) == 1:
         puppy_to_csv(data)
 
@@ -75,8 +93,3 @@ def main():
     print('Done!')
 
 main()
-
-# requests.get('http://food2fork.com/api/search?key=d6ec9ede5565ccaf1417242d82d1f537&q=shredded%20chicken')
-#r = requests.get('http://food2fork.com/api/search?key=d6ec9ede5565ccaf1417242d82d1f537&q=shredded%20chicken')
-#r.json()
-#food2fork_search('shredded chicken')
